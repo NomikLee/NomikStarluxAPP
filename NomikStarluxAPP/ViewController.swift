ec808ca84542f2a5ef190087df841d97c8469b6f
NomikLee <nomiklee@icloud.com>
2024-06-26 14:45:05 +0800

    Initial Commit

---
+++ NomikStarluxAPP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NomikStarluxAPP
+//
+//  Created by Pinocchio on 2024/6/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ NomikStarluxAPPTests/NomikStarluxAPPTests.swift
@@ -0,0 +1,17 @@
+//
+//  NomikStarluxAPPTests.swift
+//  NomikStarluxAPPTests
+//
+//  Created by Pinocchio on 2024/6/26.
+//
+
+import Testing
+@testable import NomikStarluxAPP
+
+struct NomikStarluxAPPTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
